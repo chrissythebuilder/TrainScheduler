1f0d727888f4609afcdf0dbe18e83edeeca1b388
chrissydc <bychriscelle@gmail.com>
2018-07-03 20:43:10 -0400

    html polished

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyA4zbcBdK-MgNTOYhSLbxY472sGRbek-kI",
+    authDomain: "trainscheduler-project.firebaseapp.com",
+    databaseURL: "https://trainscheduler-project.firebaseio.com",
+    projectId: "trainscheduler-project",
+    storageBucket: "trainscheduler-project.appspot.com",
+    messagingSenderId: "873192939008"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// Database Ref
+
+$("#submit").on("click", function () {
+    
+})
+
