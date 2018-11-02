f23e8c1a403b3aa06f3ec1898f22c34d59c87f89
chrissydc <bychriscelle@gmail.com>
2018-11-02 12:15:40 -0400

    table now displays all information required, momentjs logic addedin child_added function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,19 +8,15 @@
     storageBucket: "utcs-trainscheduler.appspot.com",
     messagingSenderId: "489874359973"
   };
+
   firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// Global Variables called 
-
-var name = "";
-var destination = "";
-var frequency = "";
-var nextArrival = "hh:mm";
-var minutesAway = "";
-var convertedTime = moment(time);
-
+var name;
+var destination;
+var frequency;
+var arrival;
 
 // Database Ref
 
@@ -30,16 +26,13 @@ $("#submit").on("click", function () {
     name = $("#name").val().trim();
     destination = $("#destination").val().trim();
     frequency = $("#frequency").val().trim();
-    nextArrival = $("#time").val().trim();
-    // moment JS
-    // minutesAway = $("#frequency").val().trim();
+    arrival = $("#time").val().trim();
 
     var newInfo = {
-        newName: name,
-        newDestination: destination,
-        newFrequency: frequency,
-        newArrival: nextArrival,
-        // newMinutes: minutesAWay,
+        name: name,
+        destination: destination,
+        frequency: frequency,
+        arrival: arrival,
     };
 
     database.ref().push(newInfo);
@@ -47,32 +40,78 @@ $("#submit").on("click", function () {
     console.log(name);
     console.log(destination);
     console.log(frequency);
-    console.log(nextArrival);
-    // console.log(minutesAway);
+    console.log(arrival);
 
     $("#name").val("");
     $("#destination").val("");
     $("#frequency").val("");
     $("#time").val("");
-    // $("#time").val("");
 
+    // return false;
 });
 
-    database.ref().on("child_added", function(childSnapshot) {
-        console.log(childSnapshot.val());
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
 
-        var name = childSnapshot.val().newName;
+    var newName = childSnapshot.val().name;
+    var newDestination = childSnapshot.val().destination;
+    var newFrequency = childSnapshot.val().frequency;
+    var newTime = childSnapshot.val().arrival;
+
+    console.log(newName);
+    console.log(newDestination);
+    console.log(newFrequency);
+    console.log(newTime)
+
+    var nextTime = newTime.split(":");
+    console.log(nextTime);
+
+    var trainTime = moment().hours(nextTime[0]).minutes(nextTime[1]);
+    console.log(trainTime);
+
+    var maxTime = moment.max(moment(), trainTime);
+    console.log(maxTime);
+
+    var minutes;
+    var newArrival;
+    var difference;
+    var remainder;
+
+    if (maxTime === trainTime) {
+        minutes = trainTime.diff(moment(), "minutes");
+        newArrival = trainTime.format("hh:mm A");
+    } else {
+        console.log(newFrequency);
+
+        difference = moment().diff(trainTime, "minutes");
+        console.log(difference);
+
+        remainder = newFrequency % difference; 
+        console.log(remainder);
+
+        minutes = newFrequency - remainder;
+
+        newArrival = moment().add(minutes, "m").format("hh:mm A");
+    }
+
+    console.log(minutes);
+    console.log(newArrival);
+    console.log(newFrequency);
+    console.log(difference);
+    console.log(remainder);
+    console.log("Minutes: ", minutes);
+    console.log("Arrival: ", newArrival)
 
 
-        var nextRow = $("<tr>").append(
-            $("<td>").text(name),
-            $("<td>").text(destination),
-            $("<td>").text(frequency),
-            $("<td>").text(nextArrival),
-            // $("<td>").text(minutesAway),
-        )
+    var nextRow = $("<tr>").append(
+        $("<td>").text(newName),
+        $("<td>").text(newDestination),
+        $("<td>").text(newFrequency),
+        $("<td>").text(newTime),
+        $("<td>").text(minutes),
+    )
 
-        $("#train-table > tbody").append(nextRow);
+    $("#train-table > tbody").append(nextRow);
 
 
     })
